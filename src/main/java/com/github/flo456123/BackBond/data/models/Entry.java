1bd2e48fb7e4d937f92b8e3edcdb8471b986f59a
zacha <zachappella@gmail.com>
2023-05-16 00:07:29 -0400

    restructure package organization for improved encapsulation

--- src/main/java/com/github/flo456123/BackBond/data/models/Entry.java
+++ src/main/java/com/github/flo456123/BackBond/data/models/Entry.java
@@ -1,4 +1,4 @@
-package com.github.flo456123.BackBond.repository.model;
+package com.github.flo456123.BackBond.data.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- src/main/java/com/github/flo456123/BackBond/data/repository/EntryRepository.java
+++ src/main/java/com/github/flo456123/BackBond/data/repository/EntryRepository.java
@@ -1,6 +1,6 @@
-package com.github.flo456123.BackBond.repository;
+package com.github.flo456123.BackBond.data.repository;
 
-import com.github.flo456123.BackBond.repository.model.Entry;
+import com.github.flo456123.BackBond.data.models.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EntryRepository extends JpaRepository<Entry, Integer> {
