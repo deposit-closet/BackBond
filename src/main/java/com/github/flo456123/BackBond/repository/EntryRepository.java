b8ce82f861addcaaa0781d981559b075c44b0118
zacha <zachappella@gmail.com>
2023-05-16 00:03:35 -0400

    implement EntryRepository for database query operations

---
+++ src/main/java/com/github/flo456123/BackBond/repository/EntryRepository.java
@@ -0,0 +1,7 @@
+package com.github.flo456123.BackBond.repository;
+
+import com.github.flo456123.BackBond.repository.model.Entry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntryRepository extends JpaRepository<Entry, Integer> {
+}
