0ed9495e5395954c3b7d22221ac43aa338c9bbf9
zacha <zachappella@gmail.com>
2023-05-15 23:56:09 -0400

    implement Entry POJO for representing database entries

---
+++ src/main/java/com/github/flo456123/BackBond/repository/Entry.java
@@ -0,0 +1,42 @@
+package com.github.flo456123.BackBond.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_entry")
+public class Entry {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private LocalDateTime newDate;
+
+    private double BC_1MONTH;
+    private double BC_2MONTH;
+    private double BC_3MONTH;
+    private double BC_4MONTH;
+    private double BC_6MONTH;
+    private double BC_1YEAR;
+    private double BC_2YEAR;
+    private double BC_3YEAR;
+    private double BC_5YEAR;
+    private double BC_7YEAR;
+    private double BC_10YEAR;
+    private double BC_20YEAR;
+    private double BC_30YEAR;
+
+}
