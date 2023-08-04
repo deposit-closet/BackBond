6c735953292dce90c35a3da69057b5e0c7059ac8
zacha <zachappella@gmail.com>
2023-08-04 15:04:09 -0400

    reformat code

--- src/main/java/com/github/flo456123/BackBond/BackBondApplication.java
+++ src/main/java/com/github/flo456123/BackBond/BackBondApplication.java
@@ -7,7 +7,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BackBondApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackBondApplication.class, args);
 	}
+
 }
